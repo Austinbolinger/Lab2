e62cbde960b08c26066ab9c08f191f15d94edbe5
Austin Bolinger <c16austin.bolinger@usafa.edu>
2014-02-14 15:02:22 -0700

    added the subtractor

--- Four_Bit.vhd
+++ Four_Bit.vhd
@@ -30,13 +30,14 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity Four_Bit is
-    Port ( --clk : in STD_LOGIC;
+    Port ( clk : in STD_LOGIC;
 				--buttonState : in STD_LOGIC;
 				A : in  STD_LOGIC_VECTOR (3 downto 0);
            B : in  STD_LOGIC_VECTOR (3 downto 0);
            C : in  STD_LOGIC;
            S : out  STD_LOGIC_VECTOR (3 downto 0);
-           Co : out  STD_LOGIC);
+           Co : out  STD_LOGIC;
+			  overflow : out STD_LOGIC);
 end Four_Bit;
 
 architecture Structural of Four_Bit is
@@ -49,28 +50,33 @@ component full_adder is
 			CarryOut : out STD_LOGIC);
 end component full_adder;
 
-signal L, M, N : STD_LOGIC;
+signal L, M, N,P : STD_LOGIC;
+signal Bnot,Bneg : STD_LOGIC_VECTOR(3 downto 0);
 
 --signal lastButtonState    : std_logic := '0';
 
 begin
-
+ -- C3C Weisner explained why my code was not working. He pointed out that I was 
+ --changing my signal without reassigning it
  --attempt at programming code for a subtractor
  -- found @ http://stackoverflow.com/questions/14493625/vhdl-incrementing-register-value-on-push-button-event
 -- process(clk)
 --begin
---  if(rising_edge(clk)) then
+-- if(rising_edge(clk)) then
 --    if(buttonState = '1' and lastButtonState = '0') then 
---		B <= STD_LOGIC_VECTOR (signed(not B) + 1);
---    end if;
+	 Bnot <= B when clk = '0' else
+	   STD_LOGIC_VECTOR (signed(Bnot) + 1) when clk = '1';
+--   end if;
 --    lastButtonState <= buttonState;
 --  end if;
 --end process;
  
-Bit0: full_adder Port Map(A(0),B(0),C,S(0),L);
-Bit1: full_adder Port Map(A(1),B(1),L,S(1),M);
-Bit2: full_adder Port Map(A(2),B(2),M,S(2),N);
-Bit3: full_adder Port Map(A(3),B(3),N,S(3),Co);
-
+Bit0: full_adder Port Map(A(0),Bnot(0),C,S(0),L);
+Bit1: full_adder Port Map(A(1),Bnot(1),L,S(1),M);
+Bit2: full_adder Port Map(A(2),Bnot(2),M,S(2),N);
+Bit3: full_adder Port Map(A(3),Bnot(3),N,S(3),P);
+--C3C Jonas explained the concept of overflow vs Carry out, I only had the carry out
+Co <= P;
+overflow <= P xor N; 
 end Structural;
 
